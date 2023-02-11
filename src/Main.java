2c6c84aa643ec340c6f774e1909e2690c120aae5
AA <artur.apelski@adres.pl>
2023-02-11 13:51:45 +0100

    Kalkulator etap 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,68 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
+        int pierwsza_l;
+        int druga_l;
+        char znak;
+        Scanner wej = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę:");
+        pierwsza_l = wej.nextInt();
+        System.out.println("Podaj drugą liczbę:");
+        druga_l = wej.nextInt();
+
+        System.out.println("Dodawanie dwóch liczb - wciśnij +");
+        System.out.println("Odejmowanie dwóch liczb - wciśnij -");
+        System.out.println("Mnożenie dwóch liczb - wciśnij *");
+        System.out.println("Dzielenie dwóch liczb - wciśnij /");
+        System.out.println("Operacaja modulo - wciśnij %");
+
+        znak = wej.next().charAt(0);
+        switch(znak)
+        {
+            case '+':
+            {
+                System.out.println(pierwsza_l + druga_l);
+                break;
+            }
+            case '-':
+            {
+                System.out.println(pierwsza_l - druga_l);
+                break;
+            }
+            case '*':
+            {
+                System.out.println(pierwsza_l * druga_l);
+                break;
+            }
+            case '/':
+            {
+                if(druga_l !=0)
+                {
+                    System.out.println(pierwsza_l / druga_l + "oraz reszty: " +
+                            pierwsza_l % druga_l);
+                }
+                else
+                {
+                    System.out.println("Nie dzielimy przez zero!!!");
+                }
+                break;
+            }
+            case '%':
+            {
+                if(druga_l !=0)
+                {
+                    System.out.println(pierwsza_l % druga_l);
+                }
+                else
+                {
+                    System.out.println("Nie dzielimy przez zero!!");
+                }
+            }
+
+        }
 
     }
-
 }
