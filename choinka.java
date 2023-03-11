c8ef2280edec144478c0ec9113d140051b55f205
ArturApelski <114807839+ArturApelski@users.noreply.github.com>
2023-03-11 10:37:20 +0100

    Update choinka.java

--- choinka.java
+++ choinka.java
@@ -4,12 +4,12 @@ public class choinka {
     public static void main(String[] args) {
         Scanner b = new Scanner(System.in);
         System.out.println("Daj liczbe");
-        int a = b.nextInt();
+        int a = b.nextInt(); // Podajemy wielkosć choinki podając liczbe
         for (int i = a; i >= 0; i--) {
             for (int j = i; j >= 0; j--)
                 System.out.print(" ");
             for (int k = 2*(a - i); k >= 0 ; k--)
-                System.out.print("*");
+                System.out.print("*");//Wyświetla kształt gwiazdkami
             System.out.println();
 
 
